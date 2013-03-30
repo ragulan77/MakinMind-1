172ba10e5f82a048b23adc77e6d23745e99cc3c7
ragulan77 <ragulan.charlesm@gmail.com>
2013-03-30 14:39:15 +0100

    ajout bundle JSRountingBundle

---
+++ vendor/bundles/FOS/JsRoutingBundle/Resources/js/externs.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview This file contains some properties which we don't
+ * want the compiler to rename.
+ */
+var externs = {
+    tokens: '',
+    defaults: '',
+    requirements: ''
+};
